b77560e5c8753b31651d79fbad4b17da0f03b52f
amritlink <93545493+amritlink@users.noreply.github.com>
2022-01-16 17:06:33 -0800

    Initial Commit
    1. Setup Application
    2. Create Schema and Data on Server Startup with H2 DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOMER_DETAILS (first_name, last_name, email) VALUES
+  ('Captain', 'America', 'cap@marvel.com'),
+  ('Hulk', 'Smash', 'smash@marvel.com'),
+  ('Iron', 'Man', 'ironman@marvel.com');
